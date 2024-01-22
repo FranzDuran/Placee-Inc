d117d18a87e5dbd0ad0d12d0d323e4e595e43ab4
Ariel-Alegre <arielalegre98@gmail.com>
2024-01-22 11:23:36 -0300

    update

--- server/routes/user_routers.js
+++ server/routes/user_routers.js
@@ -39,7 +39,7 @@ passport.deserializeUser((obj, done) => done(null, obj));
 router.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 
 router.get('/auth/google/callback',
-  passport.authenticate('google', { failureRedirect: '/' }),
+  passport.authenticate('google', { failureRedirect: '/error' }),
   (req, res) => {
    
 
