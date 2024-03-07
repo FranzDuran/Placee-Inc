8b01077a9b7c8f6dff4a1973ececfe4a35b64812
Ariel-Alegre <arielalegre98@gmail.com>
2024-03-07 15:01:38 -0300

    server

--- server/database/config/config.js
+++ server/database/config/config.js
@@ -35,23 +35,22 @@ module.exports = {
       }
     }
   },
-   
-/* development: {
-    dialect: 'postgres',
-    host:  'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'password',
-    database: 'turistic',
-
-  },
-  production: {
-    dialect: 'postgres',
-    host:  'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'password',
-    database: 'turistic',
-   
-  },  */ 
+  /* development: {
+      dialect: 'postgres',
+      host:  'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'password',
+      database: 'turistic',
+  
+    },
+    production: {
+      dialect: 'postgres',
+      host:  'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'password',
+      database: 'turistic',
+     
+    },  */
 }; 
